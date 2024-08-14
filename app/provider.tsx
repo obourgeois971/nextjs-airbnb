990725c0667eb1e9869c4dca1954738681e36abb
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-08-14 18:29:21 +0200

    SignOutLink Component

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,9 +1,11 @@
 'use client';
+import { Toast } from '@/components/ui/toast';
 import { ThemeProvider } from './theme-provider';
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Toast />
       <ThemeProvider
         attribute='class'
         defaultTheme='system'
--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,5 +1,18 @@
-function SignOutLink() {
-  return (
-}
+'use cient';
+import { SignOutButton } from '@clerk/nextjs';
+import { useToast } from '../ui/use-toast';
 
-export default SignOutLink
+function SignOutLink() {
+  const { toast } = useToast();
+  const handleLogout = () => {
+    toast({ description: 'You have been signed out.' });
+  };
+  return (
+    <SignOutButton redirectUrl='/'>
+      <button className='w-full text-left' onClick={handleLogout}>
+        Logout
+      </button>
+    </SignOutButton>
+  );
+}
+export default SignOutLink;
