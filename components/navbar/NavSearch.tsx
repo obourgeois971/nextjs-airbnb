1afc60782509134ce02f4681ddee8b48ea33dc43
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-08-14 16:18:20 +0200

    Logo and NavSearch Components

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,5 +1,12 @@
-function NavSearch() {
-  return <div>NavSearch</div>;
-}
+import { Input } from '../ui/input';
 
+function NavSearch() {
+  return (
+    <Input
+      type='search'
+      placeholder='find a property...'
+      className='max-w-xs dark:bg-muted'
+    />
+  );
+}
 export default NavSearch;
