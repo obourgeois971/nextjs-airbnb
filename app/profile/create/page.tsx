f84487f59707d2e3ba50d4ae290b26ab08432b5e
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-08-14 20:17:36 +0200

    SubmiButton Component

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,6 +1,6 @@
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
+// import { Button } from '@/components/ui/button';
 
 const createProfileAction = async (formData: FormData) => {
   'use server';
@@ -14,13 +14,13 @@ function CreateProfilePage() {
       <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
       <div className='border p-8 rounded-md max-w-lg'>
         <form action={createProfileAction}>
-          <div className='mb-2'>
+          {/* <div className='mb-2'>
             <Label htmlFor='firstName'>First Name</Label>
             <Input id='firstName' name='firstName' type='text' />
-          </div>
-          <Button type='submit' size='lg'>
+          </div> */}
+          {/* <Button type='submit' size='lg'>
             Create Profile
-          </Button>
+          </Button> */}
         </form>
       </div>
     </section>
