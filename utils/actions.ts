fcf8d10571c84b45d243987259d1d4e6d664829e
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-08-15 12:04:25 +0200

    Fetch User Profile

--- utils/actions.ts
+++ utils/actions.ts
@@ -53,3 +53,31 @@ export const fetchProfileImage = async () => {
   });
   return profile?.profileImage;
 };
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+  if (!profile) return redirect('/profile/create');
+  return profile;
+};
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error('You must be logged in to access this route');
+  }
+  if (!user.privateMetadata.hasProfile) redirect('/profile/create');
+  return user;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  return { message: 'update profile action' };
+};
