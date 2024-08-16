1e6d7e5289e6df553e65b80cf2a2dc6f99ed83bb
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-08-16 11:04:08 +0200

    Fetch Favorite

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -2,15 +2,21 @@ import { FaHeart } from 'react-icons/fa';
 import { Button } from '@/components/ui/button';
 import { auth } from '@clerk/nextjs/server';
 import { CardSignInButton } from '../form/Buttons';
+import { fetchFavoriteId } from '@/utils/actions';
+import FavoriteToggleForm from './FavoriteToggleForm';
 
-function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
+async function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
   const { userId } = auth();
   if (!userId) return <CardSignInButton />;
-  return (
-    <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
-      <FaHeart />
-    </Button>
-  );
+  const favoriteId = await fetchFavoriteId({ propertyId });
+
+  // return (
+  //   <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
+  //     <FaHeart />
+  //   </Button>
+  // );
+
+  return <FavoriteToggleForm favoriteId={favoriteId} propertyId={propertyId} />;
 }
 
 export default FavoriteToggleButton;
